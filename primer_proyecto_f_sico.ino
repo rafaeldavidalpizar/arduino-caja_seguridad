a747cab31fc9ac66ce7106450af2ab6a47b5cc0e
Rafael David Alpízar <rafael.david.alpizar@gmail.com>
2025-11-13 17:19:10 -0600

    Update primer_proyecto_f_sico.ino

--- primer_proyecto_f_sico.ino
+++ primer_proyecto_f_sico.ino
@@ -14,15 +14,14 @@ char teclas[FILAS][COLUMNAS] = {
 };
 byte pinesFilas[FILAS] = {2, 3, 4, 5};
 byte pinesColumnas[COLUMNAS] = {6, 7, 8, 9};
-
-Keypad teclado = Keypad(makeKeypad(teclas), pinesFilas, pinesColumnas, FILAS, COLUMNAS);
+Keypad teclado = Keypad(makeKeymap(teclas), pinesFilas, pinesColumnas, FILAS, COLUMNAS);
 
 String clave = "1234";  //Cambiar aquí la clave
 String entrada = "";
 
 void setup(){
   pestillo.attach(10);
-  pestillo.write(90);  // Pocision inicial CERRADO
+  pestillo.write(90);  // Pocision inicial CERRADO  
   pinMode(LED_VERDE, OUTPUT);
   pinMode(LED_ROJO, OUTPUT);
   Serial.begin(9600);
@@ -34,6 +33,39 @@ void loop(){
 
   if (tecla){
     Serial.print("Tecla presionada: ");
-    Serial.println(tecla);                             
+    Serial.println(tecla);
+
+    if (tecla == '#'){  //tecla para comfirmar
+    if (entrada == clave){
+      abrirCaja();
+    } else {
+      errorClave();
+    }
+    entrada = "";
+    } else if (tecla == '*'){ // borrar
+      entrada = "";
+    } else {
+      entrada += tecla;
+    }        
   }
 }
+
+void abrirCaja(){
+  digitalWrite(LED_VERDE, HIGH);
+  digitalWrite(LED_ROJO, LOW);
+  pestillo.write(0);
+  delay(3000); //Modificar tiempo abierto aqui
+  pestillo.write(90);
+  digitalWrite(LED_VERDE, LOW);
+}
+
+void errorClave(){
+  for (int i = 0; i < 3; i++){
+    digitalWrite(LED_ROJO, HIGH);
+    delay(200);
+    digitalWrite(LED_ROJO, LOW);
+    delay(200);
+  }
+}
+
+
