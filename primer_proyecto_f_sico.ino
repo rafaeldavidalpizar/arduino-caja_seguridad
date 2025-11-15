5504334dac6a7c74a35ed8f7ccbb6881c2d6a098
Rafael David Alpízar <rafael.david.alpizar@gmail.com>
2025-11-14 19:36:27 -0600

    Sistema funcionando

--- primer_proyecto_f_sico.ino
+++ primer_proyecto_f_sico.ino
@@ -7,10 +7,10 @@ Servo pestillo;
 const byte FILAS = 4;
 const byte COLUMNAS = 4;
 char teclas[FILAS][COLUMNAS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+  {'A', '9', '5', '1'},
+  {'B', '0', '6', '2'},
+  {'C', '*', '7', '3'},
+  {'D', '#', '8', '4'}
 };
 byte pinesFilas[FILAS] = {2, 3, 4, 5};
 byte pinesColumnas[COLUMNAS] = {6, 7, 8, 9};
@@ -18,14 +18,15 @@ Keypad teclado = Keypad(makeKeymap(teclas), pinesFilas, pinesColumnas, FILAS, CO
 
 String clave = "1234";  //Cambiar aquí la clave
 String entrada = "";
+int ALARM_SOUND = 11;
 
 void setup(){
   pestillo.attach(10);
   pestillo.write(90);  // Pocision inicial CERRADO  
   pinMode(LED_VERDE, OUTPUT);
   pinMode(LED_ROJO, OUTPUT);
+  pinMode(ALARM_SOUND, OUTPUT);
   Serial.begin(9600);
-  
 }
 
 void loop(){
@@ -54,6 +55,12 @@ void abrirCaja(){
   digitalWrite(LED_VERDE, HIGH);
   digitalWrite(LED_ROJO, LOW);
   pestillo.write(0);
+  for (int i=0; i<5; i++){
+    digitalWrite(ALARM_SOUND, HIGH);
+    delay(100);
+    digitalWrite(ALARM_SOUND, LOW);
+    delay(100);
+  }
   delay(3000); //Modificar tiempo abierto aqui
   pestillo.write(90);
   digitalWrite(LED_VERDE, LOW);
@@ -62,6 +69,12 @@ void abrirCaja(){
 void errorClave(){
   for (int i = 0; i < 3; i++){
     digitalWrite(LED_ROJO, HIGH);
+  for (int i=0; i<2; i++){
+    digitalWrite(ALARM_SOUND, HIGH);
+    delay(50);
+    digitalWrite(ALARM_SOUND, LOW);
+    delay(100);
+  }
     delay(200);
     digitalWrite(LED_ROJO, LOW);
     delay(200);
